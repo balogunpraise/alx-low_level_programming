c225d606f77b24f97c979f194ec2dfb4a401ad23
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-16 12:35:23 -0800

    added description for return value

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - this is the main program
  *
- * Return : 0
+ * Return : program returns 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

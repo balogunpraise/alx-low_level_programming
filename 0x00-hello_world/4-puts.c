d1068ab435a9a64c52299ec12587ede6eb2bbbb9
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-16 09:40:37 -0800

    modified code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+/**
+ * prints a message to the screen
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }

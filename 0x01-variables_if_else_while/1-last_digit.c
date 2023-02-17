0f925ba0e2d0ae115f542f37a108792011735a5a
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-17 15:01:51 -0800

    forbids declaration and code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit = n % 10;
+	int last_digit;
+	last_digit = n % 10;
 	if (last_digit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	else if (last_digit > 5)

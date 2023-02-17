6ab5717d73d1ca3c8b8c103682523420166ba910
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-17 15:31:17 -0800

    trying mem management

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,6 +20,6 @@ int main(void)
 	else if (last > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	else if (last < 6 && last != 0)
-		printf("Last digit of %d is %d and is less that 6 and not 0\n", n, last);
+		printf("Last digit of %d is %s and is less that 6 and not 0\n", n, last);
 	return (0);
 }

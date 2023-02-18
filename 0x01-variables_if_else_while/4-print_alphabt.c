7f5cd4f4e779c1071647ae43bf573f56f677123f
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-17 16:17:00 -0800

    print letters excluding e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - prints alphabets excluding e and q
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		if (letter == 'e' || letter == 'q')
+			continue;
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}

894bb5319670ea60999414ec9717a28606b6dcae
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-21 07:21:14 -0800

    checking cas

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - checks case
+ * Description: 'returns zero or one'
+ * Return: 0 || 1
+ */
+int _islower(int c)
+{
+	char first;
+	char last;
+
+	first = 'a';
+	last = 'z';
+	if (c >= first || c <= last)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

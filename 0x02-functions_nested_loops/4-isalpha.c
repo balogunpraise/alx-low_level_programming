93207e18b96638f0218b9ff44cf06d1a33bf29fb
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-21 09:04:00 -0800

    work

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -13,11 +13,13 @@ int _isalpha(int c)
 	char lower_last;
 	char upper_last;
 
+	bool islower = c >= lower_first && c <= lower_last;
+	bool isupper = c >= upper_first && c <= upper_last;
 	lower_first = 'a';
 	upper_first = 'A';
 	lower_last = 'z';
 	upper_last = 'Z';
-	if ((c >= lower_first && c <= lower_last) || (c >= upper_first && c <= upper_last))
+	if (islower || isupper)
 		return (1);
 	else
 		return (0);

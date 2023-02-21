684d1e26ccc9a2b45f43f22ef2fce13c2ab816bc
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-21 07:57:18 -0800

    check alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks alphabet
+ * Description: 'checks if a character is an alphabet'
+ * @c: value
+ * Return: 0 || 1
+ */
+int _isalpha(int c)
+{
+	char lower_first;
+	char upper_first;
+	char lower_last;
+	char upper_last;
+
+	lower_first = 'a';
+	upper_first = 'A';
+	lower_last = 'z';
+	upper_last = 'Z';
+
+	if ((c >= lower_first && c <= lower_last) || (c >= upper_first && c <= upper_last))
+	{
+		return (1);
+	}
+	else{
+		return (0);
+	}
+}
+

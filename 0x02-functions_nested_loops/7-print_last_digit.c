9b53ebde4ab1eac43182d7a28ab11c98c37782b4
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-21 14:40:27 -0800

    got abs value

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,12 @@ int print_last_digit(int n)
 	int x;
 
 	x = n % 10;
-	_putchar(x + '0');
-	return (x);
+	if (x < 0)
+	{
+		_putchar(x * -1 + '0');
+		return (x * -1);
+	}
+	else{
+		_putchar(x * -1 + '0');
+		return (x);
 }

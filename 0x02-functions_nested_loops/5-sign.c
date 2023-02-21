2908055a5a89bb979012ef41bdd155e189d0eef9
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-21 09:41:12 -0800

    removed empty spaces

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,11 +4,11 @@
  * print_sign - prints the sign of a number
  * Description: This prints a negative or positive
  * @n: value
- * Return 0 || 1
+ * Return: 0 || 1
  */
 int print_sign(int n)
 {
-	if ( n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		return (-1);

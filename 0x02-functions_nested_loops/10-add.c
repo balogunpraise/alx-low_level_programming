f4e3a18968cb15ade7c03f9ed839d9217eee66b6
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-21 16:48:04 -0800

    sum function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of two numbers
+ * @a: value1
+ * @b: value2
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}

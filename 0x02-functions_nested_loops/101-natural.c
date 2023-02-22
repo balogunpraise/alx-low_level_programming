65767ba0bf425c88a1657221ed171cef4becfa15
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-22 09:12:07 -0800

    fixed logic

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * natural_numbers - prints the multiples of three or five
+ * main - prints the multiples of three or five
  * Return: Always 0
  */
 int main(void)
@@ -10,10 +10,11 @@ int main(void)
 	int sum;
 
 	n = 1024;
-	while (n <1024)
+	while (n < 1024)
 	{
-		if(n % 3 == 0 || n % 5 == 0)
-			sum = sum + n;
+		if (n % 3 == 0 || n % 5 == 0)
+			sum += n;
+		n++;
 	}
 	printf("%d", sum);
 	return (0);

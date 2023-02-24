c1a62cdb03ed3004ef0b7dee2be8a76d652bbbc4
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-02-24 08:08:30 -0800

    print numbers excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers excluding 2 and 4
+ * Return: Always void
+ */
+void print_most_numbers(void)
+{
+	int count;
+
+	count = 0;
+	while (count <= 9)
+	{
+		if ((count != 2) || (count != 4))
+		{
+			_putchar(count + '0');
+		}
+		count++;
+	}
+	_putchar('\n');
+}
+

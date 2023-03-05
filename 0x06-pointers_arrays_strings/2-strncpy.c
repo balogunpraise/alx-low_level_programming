9157ba78af8758e81c9c47528d665d2255d35a57
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-03-05 06:36:25 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strncpy - Copy
- * @dest: to
- * @src: from
- * @n: number
+ * _strncpy - Entry point
+ * @dest: copy to
+ * @src: copy from
+ * @n: input number of char
  * Return: Always 0 (Success)
  */
 char *_strncpy(char *dest, char *src, int n)
@@ -12,11 +12,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
-	{
 		if (i < n)
 			dest[i] = src[i];
 		while (i < n)
 			dest[i++] = '\0';
-	}
+
 	return (dest);
 }

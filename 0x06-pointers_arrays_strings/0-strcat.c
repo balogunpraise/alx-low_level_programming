6b356a2d4eacd7ea4914d75f55bb7b49c404e3fc
balogunpraise <balogunpraise2git config --global user.email balogunpraise2@gmail.com>
2023-03-05 06:32:32 -0800

    first one

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,20 +2,22 @@
 
 /**
  * _strcat - a function that concatenates two strings
- * @dest: to
- * @src: from
- * Return: concat strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: two concat strings
  */
 char *_strcat(char *dest, char *src)
 {
-   int i, j;
+	int i, n;
 
-   for (i = 0; dest[i] != '\0'; i++)
-   {
-   }
-   for (j = 0; (dest[i + j] = *src++) != '\0'; j++)
-   {
-   }
-   return (dest); 
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+
+	return (dest);
 }
 

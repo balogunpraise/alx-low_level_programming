b17a6aee5420ede13e496515d3dc76af979647e9
balogunpraise <balogunpraise2@gmail.com>
2023-04-05 09:12:20 -0700

    return the address trhough a malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * malloc_checked - Returns the size
+ * @b: parameter
+ * Return: address to the newly created item
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}

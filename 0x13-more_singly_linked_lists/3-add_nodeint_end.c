450286195a0b1cddcbba4ebcf15075123e882a8b
balogunpraise <balogunpraise2@gmail.com>
2023-04-05 15:02:45 -0700

    add node to end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - Start Point
+ * @head: start
+ * @n: input
+ * Return: 0
+ */
+listint_t *add_nodeint_end(listint_t **head const int n)
+{
+	listint_t *fnode;
+	listint_t *snode;
+
+	fnode = malloc(sizeof(listint_t));
+	if (fnode == NULL)
+		return (NULL);
+	fnode->item = n;
+	fnode->next = NULL;
+	if (*head == NULL)
+	{
+		*head = fnode;
+		return (fnode);
+	}
+	snode = *head;
+	while (snode->next != NULL)
+		snode = snode->next;
+	snode->next = fnode;
+	return (fnode);
+}
